9f8bacb08f69048580c98fe9991172a96680f452
kdkwakaba <kdkwakaba@gmail.com>
2023-01-15 22:59:38 +0900

    Add next app.

---
+++ Azure/staticwebapps/blog_20230117/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Azure Static Web Apps Integration API</title>
+      </Head>
+      <main>
+        Hello World!
+      </main>
+    </>
+  )
+}
